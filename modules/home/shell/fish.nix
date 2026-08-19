eae2c9179f1a04ece768e7c238e0e532bdaa82c8
Aung Koko Lwin <toyko2001@gmail.com>
2026-08-19 20:49:54 +0630

    Add fish aliases and rebuild abbr

--- modules/home/shell/fish.nix
+++ modules/home/shell/fish.nix
@@ -1,4 +1,25 @@
 { ... }:
 {
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+
+    # alias = shadow a command with flags you always want.
+    shellAliases = {
+      ll = "eza -lh  --icons --group-directories-first --git";
+      la = "eza -lha --icons --group-directories-first --git";
+      lt = "eza --tree --level=2 --icons";
+      vim = "nvim";
+      top = "btop";
+      lgit = "lazygit";
+      ldocker = "lazydocker";
+      zj = "zellij";
+      df = "df -h";
+      free = "free -h";
+    };
+
+    # abbr = expands to editable text before it runs.
+    shellAbbrs = {
+      rebuild = "sudo nixos-rebuild switch --flake ~/.dotfiles#genius";
+    };
+  };
 }
